4c0208f65d3c5e4090f94df50bb3c7611f8f15aa
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-04-12 10:42:51 +0300

    TS study commit

---
+++ w002/w007_array.js
@@ -0,0 +1,12 @@
+var list1 = [1, 3, 5, 7, 9];
+var list2 = [1, 3, 5, 7, 9];
+console.log(list1[0]);
+console.log(list2[0]);
+console.log(list1[0] + list2[0]);
+list1[0] = 10;
+console.log(list1[0] + list2[0]);
+console.log(list1[0] + list1[1]);
+console.log(list1[0] + list1[1] + list1[2]);
+console.log(list1[0] + list1[1] + list1[2] + list1[3]);
+console.log(list1[0] + list1[1] + list1[2] + list1[3] + list1[4]);
+console.log(list1[0] + list1[1] + list1[2] + list1[3] + list1[4] + list1[5]);
