19397f357c6f138126245f393d64236b0eb37c4d
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-04-12 10:55:11 +0300

    TS study commit

--- w002/w009_interface.js
+++ w002/w009_interface.js
@@ -2,3 +2,15 @@ function add(values) {
     return values.value1 + values.value2;
 }
 console.log(add({ value1: 10, value2: 20 }));
+function subtract(values) {
+    return values.value1 - values.value2;
+}
+console.log(subtract({ value1: 10, value2: 20 }));
+function multiply(values) {
+    return values.value1 * values.value2;
+}
+console.log(multiply({ value1: 10, value2: 20 }));
+function divide(values) {
+    return values.value1 / values.value2;
+}
+console.log(divide({ value1: 10, value2: 20 }));
--- w002/w009_interface.ts
+++ w002/w009_interface.ts
@@ -9,3 +9,20 @@ function add(values: CalculatorValues) {
 
 console.log(add({value1: 10, value2: 20}));
 
+function subtract(values: CalculatorValues) {
+    return values.value1 - values.value2;
+}
+
+console.log(subtract({value1: 10, value2: 20}));
+
+function multiply(values: CalculatorValues) {
+    return values.value1 * values.value2;
+}
+
+console.log(multiply({value1: 10, value2: 20}));
+
+function divide(values: CalculatorValues) {
+    return values.value1 / values.value2;
+}
+
+console.log(divide({value1: 10, value2: 20}));
